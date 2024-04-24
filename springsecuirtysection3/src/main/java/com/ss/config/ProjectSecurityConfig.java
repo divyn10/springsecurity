ea8a551cd584497a3f17d127894e7d01f160d90f
divyanshu <divyanshugairola@gmail.com>
2024-04-24 22:55:04 +0530

    created InMemoryUsers

--- springsecuirtysection3/src/main/java/com/ss/config/ProjectSecurityConfig.java
+++ springsecuirtysection3/src/main/java/com/ss/config/ProjectSecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -13,33 +16,27 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        /*
-        Below is th Custom security configuration
-        */
-
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount", "myBalance", "/myLoans", "myCards").authenticated()
                 .requestMatchers("/notices", "/contact").permitAll())
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults());
         return http.build();
+    }
 
-        /*
-        Configurations to deny all the requests
-         */
-        /*http.authorizeHttpRequests((requests) -> requests
-                        .anyRequest().denyAll())
-                .formLogin(withDefaults())
-                .httpBasic(withDefaults());
-        return http.build();*/
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
 
-        /*
-        Configurations to permit all the requests
-         */
-       /* http.authorizeHttpRequests((requests) -> requests
-                        .anyRequest().permitAll())
-                .formLogin(withDefaults())
-                .httpBasic(withDefaults());
-        return http.build();*/
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
