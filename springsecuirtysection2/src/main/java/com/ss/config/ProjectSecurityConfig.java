61a80ff56f39bc328da82a57101e3d2efa67818b
divyanshu <divyanshugairola@gmail.com>
2024-04-24 14:20:52 +0530

    custom security configuration added

---
+++ springsecuirtysection2/src/main/java/com/ss/config/ProjectSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.ss.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        /*
+        Below is the custom security configuration.
+         */
+        /*
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+         */
+    }
+}
